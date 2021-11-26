134d9ccd21a027aa9135ccdd1f116eafb3ba7542
daiyifan <252127196@qq.com>
2021-11-26 15:41:01 +0800

    删除auth模块，增加authmanager模块

--- mozi-authmanager/src/main/java/com/dai/moziauthmanager/MoziAuthmanagerApplication.java
+++ mozi-authmanager/src/main/java/com/dai/moziauthmanager/MoziAuthmanagerApplication.java
@@ -1,13 +1,13 @@
-package com.example.moziauth;
+package com.dai.moziauthmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoziAuthApplication {
+public class MoziAuthmanagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MoziAuthApplication.class, args);
+        SpringApplication.run(MoziAuthmanagerApplication.class, args);
     }
 
 }
