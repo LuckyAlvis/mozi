b72eb27d4435a83321f9e704e98a9375dbf7a8ea
dyf_Home <252127196@qq.com>
2021-12-14 22:48:46 +0800

    测试Template

--- mozi-admin/mozi-admin-server/src/main/java/com/dai/mozi/MoziAdminServerApplication.java
+++ mozi-admin/mozi-admin-server/src/main/java/com/dai/mozi/MoziAdminServerApplication.java
@@ -2,6 +2,8 @@ package com.dai.mozi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MoziAdminServerApplication {
@@ -10,4 +12,8 @@ public class MoziAdminServerApplication {
         SpringApplication.run(MoziAdminServerApplication.class, args);
     }
 
+    @Bean
+    RestTemplate template() {
+        return new RestTemplate();
+    }
 }
--- mozi-admin/mozi-admin-server/src/main/java/com/dai/mozi/admin/controller/UserController.java
+++ mozi-admin/mozi-admin-server/src/main/java/com/dai/mozi/admin/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.dai.mozi.admin.controller;
 
 import com.dai.mozi.admin.po.User;
+import com.dai.mozi.admin.po.pojo.Data;
+import com.dai.mozi.admin.po.pojo.JsonRootBean;
 import com.dai.mozi.admin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -13,6 +16,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/list")
     public List<User> getUserList() {
@@ -28,5 +34,20 @@ public class UserController {
     @PostMapping("login")
     public String login(@RequestBody User user) {
         return userService.login(user);
+    }
+
+    @GetMapping("/list1/{bvid}")
+    public Boolean getBilibili(@PathVariable("bvid") String bvid) {
+        try {
+            JsonRootBean jsonRootBean = restTemplate.getForObject("https://api.bilibili.com/x/player/pagelist?bvid=" + bvid + "&jsonp=jsonp", JsonRootBean.class);
+            assert jsonRootBean != null;
+            for (Data data : jsonRootBean.getData()) {
+                System.out.println(data.getPart() + "\t" + String.format("%.1f", (double) data.getDuration() / 60));
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+
     }
 }
